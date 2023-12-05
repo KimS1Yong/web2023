21cd2abcbac12fbf89a6b806fe4ff408dd9335aa
Siyong Kim <sdragon0928@naver.com>
2023-12-05 09:04:25 +0900

    login_test

--- index0.js
+++ index0.js
@@ -1,7 +1,7 @@
 let currentPage = 0;
 
 function showAlert() {
-   alert('해다 리그에 속한 한국인 선수가 없습니다.');
+   alert('해당 리그에 속한 한국인 선수가 없습니다.');
 }
 
 function showEPL() {
@@ -14,4 +14,12 @@ function showBUNDES() {
 
 function showLIGUE1() {
    window.location.href=`C:/Users/user/OneDrive/Desktop/고급웹 프로젝트/ligue1/ligue1_index1.html`
+}
+
+function showLogin() {
+   window.location.href=`C:/Users/user/OneDrive/Desktop/고급웹 프로젝트/login/login.html`
+}
+
+function showRegister() {
+   window.location.href=`C:/Users/user/OneDrive/Desktop/고급웹 프로젝트/login/register.html`
 }
