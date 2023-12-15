ba49786717dc161e8203d7234e6e8662ee244457
KimS1Yong <113281522+KimS1Yong@users.noreply.github.com>
2023-12-15 21:51:03 +0900

    Update login.sql

--- login.sql
+++ login.sql
@@ -2,14 +2,19 @@
 CREATE DATABASE IF NOT EXISTS web CHARACTER SET utf8 COLLATE utf8_bin;
 USE web;
 
-CREATE TABLE IF NOT EXISTS user (
-  id int(12) NOT NULL,
+CREATE TABLE user (
+  id int(12) NOT NULL AUTO_INCREMENT=1 PRIMARY KEY,
   username varchar(50) NOT NULL,
   password varchar(255) NOT NULL,
   email varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-alter table user modify id int not null auto_increment primary key;
+)
 
 INSERT INTO user (id, username, password, email) VALUES (1, 'test', 'test', 'test@test.com');
 INSERT INTO user (username, password, email) VALUES ('test2', 'test2', 'test2@test.com');
+
+CREATE TABLE web.cheer (
+  post_id int NOT NULL AUTO_INCREMENT=1 PRIMARY KEY,
+  content TEXT
+)
+
+INSERT INTO web.cheer (content) VALUES('손흥민 선수 화이팅!');
